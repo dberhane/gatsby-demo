d737ab98ed78a2c66fa67cf47636f38d9fe970be
Daniel Berhane <DBERHANE@bmj.com>
2018-04-19 14:19:14 +0100

    improvements to mostread feed widget

--- src/components/MostRead.js
+++ src/components/MostRead.js
@@ -1,21 +1,21 @@
 import React from "react"
 import Link from "gatsby-link"
-
+import { List, Header } from "semantic-ui-react"
 class MostRead extends React.Component {
 
     render () {
+
         const mostReadItems = this.props.gdata.allDataJson.edges[0].node.bmjsiteData
-        console.log(mostReadItems)
+        const websiteUrl = this.props.gdata.allDataJson.edges[0].node.bmjsiteHead[0].websiteUrl
         
         return (
-            <div>
-              <h2>Most Read Widget</h2>  
-              <ul>
-
+            <div style={{paddingBottom:'2.5em'}}>
+              <Header as='h2'>BMJ Open most read</Header>  
+              <List ordered divided relaxed>
                   {mostReadItems.map((item,i) => 
-                      <li key={item.pagePath}>{item.pageTitle}</li>
+                      <List.Item key={item.pagePath}><a href={ `${websiteUrl}${item.pagePath}` }>{item.pageTitle}</a></List.Item>
                   )}
-              </ul>
+              </List>
             </div>
         )
     }
--- src/templates/index.js
+++ src/templates/index.js
@@ -75,6 +75,7 @@ export const query = graphql`
             accountId
             webPropertyId
             name
+            websiteUrl
           }
           bmjsiteData {
             pagePath
