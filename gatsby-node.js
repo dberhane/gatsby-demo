c170567c40671b76da96dc67ede18a050348ab71
Daniel Berhane <dberhane@bmj.com>
2018-04-20 18:27:21 +0100

    code refractoring

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,78 +2,26 @@ const _ = require(`lodash`)
 const Promise = require(`bluebird`)
 const path = require(`path`)
 const slash = require(`slash`)
+const allQueries = require(`./src/queries/allQueries`)
 const createPaginatedPages = require("gatsby-paginate")
 
-const pageQuery = `
-{
-  allWordpressPage {
-    edges {
-      node {
-        id
-        slug
-        status
-        template
-      }
-    }
-  }
-}
-`
-
-const postsQuery = `
-{
-  allWordpressPost {
-    edges {
-      node {
-        id
-        slug
-        title
-        excerpt
-        status
-        date(formatString: "MMMM DD, YYYY")
-        template
-        format
-      }
-    }
-  }
-}
-`
-
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
-// Will create pages for Wordpress pages (route : /{slug})
-// Will create pages for Wordpress posts (route : /post/{slug})
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
+  const { createPage } = boundActionCreators;
+
   return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local Wordpress graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
+    // Templates
+    const pageTemplate = path.resolve("./src/templates/page.js");
+    const postTemplate = path.resolve("./src/templates/post.js");
 
-    // ==== PAGES (WORDPRESS NATIVE) ====
-    graphql(pageQuery)
-      .then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
+    resolve(
+      graphql(allQueries).then(result => {
+        if (result.errors) reject(result.errors)
 
-        // Create Page pages.
-        const pageTemplate = path.resolve(`./src/templates/page.js`)        
-        // We want to create a detailed page for each
-        // page node. We'll just use the Wordpress Slug for the slug.
-        // The Page ID is prefixed with 'PAGE_'
-        _.each(result.data.allWordpressPage.edges, edge => {
-          // Gatsby uses Redux to manage its internal state.
-          // Plugins and sites can use functions like "createPage"
-          // to interact with Gatsby.
+        // Pages detail
+        const pages = result.data.allWordpressPage.edges
+
+        pages.forEach(edge => {
           createPage({
-            // Each page is required to have a `path` as well
-            // as a template component. The `context` is
-            // optional but is often necessary so the template
-            // can query data specific to each page.
             path: `/${edge.node.slug}/`,
             component: slash(pageTemplate),
             context: {
@@ -81,52 +29,29 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
             },
           })
         })
-      })
-      // ==== END PAGES ====
 
-      // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
-      .then(() => {
-        graphql(postsQuery).then(result => {
-          if (result.errors) {
-            console.log(result.errors)
-            reject(result.errors)
-          }
-          const postTemplate = path.resolve(`./src/templates/post.js`)
-          const postsTemplate = path.resolve(`./src/templates/index.js`)
+        // Posts detail
+        const posts = result.data.allWordpressPost.edges
 
-          // Create Posts
-/*          createPage({
-            path: `/posts/`,
-            component: slash(postsTemplate)
-          })
-*/        
-
-          createPaginatedPages({
-            edges: result.data.allWordpressPost.edges,
-            createPage: createPage,
-            pageTemplate: "src/templates/index.js",
-            pageLength: 10,
-            pathPrefix: ""
-          })
-
-          // We want to create a detailed page for each
-          // post node. We'll just use the Wordpress Slug for the slug.
-          // The Post ID is prefixed with 'POST_'
-          _.each(result.data.allWordpressPost.edges, edge => {
-            createPage({
-              path: `/post/${edge.node.slug}`,
-              component: slash(postTemplate),
-              context: {
-                id: edge.node.id,
-              },
-            })
-          })
-
-
-
-          resolve()
+        createPaginatedPages({
+          edges: posts,
+          createPage: createPage,
+          pageTemplate: "src/templates/index.js",
+          pageLength: 3,
+          pathPrefix: ""
         })
+
+        posts.forEach(edge => {
+          createPage({
+            path: `/post/${edge.node.slug}/`,
+            component: slash(postTemplate),
+            context: {
+              id: edge.node.id,
+            },
+          });
+        })
+
       })
-    // ==== END POSTS ====
-  })
-}
+    )
+  });
+};
---
+++ src/queries/allQueries.js
@@ -0,0 +1,29 @@
+module.exports = `
+    {
+        allWordpressPage {
+            edges {
+              node {
+                id
+                slug
+                status
+                template
+              }
+            }
+        }
+        
+        allWordpressPost {
+            edges {
+              node {
+                id
+                slug
+                title
+                excerpt
+                status
+                date(formatString: "MMMM DD, YYYY")
+                template
+                format
+              }
+            }
+          }
+    }
+`
