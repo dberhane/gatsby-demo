e2a14eb4bfe8a5d2cdbc78c0ba9e8ae7a5424c5f
Daniel Berhane <dberhane@bmj.com>
2018-04-20 18:28:58 +0100

    index page for posts increased to 10

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,7 +37,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
           edges: posts,
           createPage: createPage,
           pageTemplate: "src/templates/index.js",
-          pageLength: 3,
+          pageLength: 10,
           pathPrefix: ""
         })
 
